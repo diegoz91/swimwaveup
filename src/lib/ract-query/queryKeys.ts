d22a99df04d3061c6dabf61d6f3c6292584012c4
Drugo <d.zago.1991@gmail.com>
2023-12-18 14:05:19 +0100

    new changes

---
+++ src/lib/ract-query/queryKeys.ts
@@ -0,0 +1,29 @@
+export enum QUERY_KEYS {
+    // AUTH KEYS
+    CREATE_USER_ACCOUNT = "createUserAccount",
+  
+    // USER KEYS
+    GET_CURRENT_USER = "getCurrentUser",
+    GET_USERS = "getUsers",
+    GET_USER_BY_ID = "getUserById",
+  
+    // POST KEYS
+    GET_POSTS = "getPosts",
+    GET_INFINITE_POSTS = "getInfinitePosts",
+    GET_RECENT_POSTS = "getRecentPosts",
+    GET_POST_BY_ID = "getPostById",
+    GET_USER_POSTS = "getUserPosts",
+    GET_FILE_PREVIEW = "getFilePreview",
+
+    // FOLLOW KEYS
+
+    GET_FOLLOWERS = "getFollowers",
+    GET_INFINITE_FOLLOWERS = "getInfiniteFollowers",
+    GET_RECENT_FOLLOWERS = "getRecentFollowers",
+    GET_FOLLOWERS_BY_ID = "getFollowersById",
+    GET_USER_FOLLOWERS = "getUserFollowers",
+
+
+    //  SEARCH KEYS
+    SEARCH_POSTS = "getSearchPosts",
+  }
